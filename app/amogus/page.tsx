7e792d9775cafc03930906c6385353e19bb00266
Zack <zackryrosli69@gmail.com>
2024-04-17 02:06:41 +0800

    Update page.tsx

--- app/amogus/page.tsx
+++ app/amogus/page.tsx
@@ -1,11 +1,11 @@
 import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
     const urls = ["https://youtu.be/dQw4w9WgXcQ?si=Sjy9hdDKQn2XOink"];
 
-    setTimeout(() => {
-        redirect(urls[Math.floor(Math.random() * urls.length)]);
-    }, 3000);
+    await new Promise(resolve => setTimeout(resolve, 3000));
+
+    redirect(urls[Math.floor(Math.random() * urls.length)]);
     
     return null;
 }
