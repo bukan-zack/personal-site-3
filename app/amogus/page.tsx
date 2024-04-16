2312fa37d3a47e48396a7e189d6c07a3ccdc9e3c
Zack <zackryrosli69@gmail.com>
2024-04-17 01:44:17 +0800

    Update page.tsx

--- app/amogus/page.tsx
+++ app/amogus/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
-export default async function Page() {
+export default function Page() {
     const urls = ["https://youtu.be/dQw4w9WgXcQ?si=Sjy9hdDKQn2XOink"];
 
     setTimeout(() => {
